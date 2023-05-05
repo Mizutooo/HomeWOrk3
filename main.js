31b167b5b2c195ea6489ddbf44d7bace2e63c415
Mizutooo <berkuttopcubaev@gmail.com>
2023-05-05 12:10:24 +0600

    darova зайбал

---
+++ main.js
@@ -0,0 +1,59 @@
+// function compare(number1, number2) {
+//     if(number1 < number2) {
+//         console.log('первое меньше. ха-ха-ха');
+//     }else if(number2 < number1) {
+//         console.log('второе число меньше. да ладно');
+//     }else console.log('оба числа равны. я так и знал!');
+// }
+
+
+// 2 задание 
+
+
+// console.log(compare(5,5));
+// console.log(compare(6,1));
+
+// function degree (num, degree) {
+//     return num**degree;
+//     return num**degree;
+// }
+
+// console.log(degree(3, 2));
+
+// 3 задание    
+
+// const sumOfNumbers = (numbers) => {
+//  return numbers.reduce((a, b) => a + b)
+// }
+// console.log(sumOfNumbers([2, 3, 6, 3, 7]));
+
+//доп задание 
+
+// 5 задание 
+
+// const bankAccount = {
+//     accountNumber: 0,
+//     balance: 0,
+//     sendTransfer: function (accountNumber, balance) {
+//         this.balance = balance
+//         this.accountNumber = accountNumber
+//     }
+// }
+
+// bankAccount.sendTransfer(45335654645, 20)
+// console.log(bankAccount.accountNumber, bankAccount.ba);
+
+//4 задание 
+
+//дополнительные
+
+// const user = {
+//     phoneNumber: '+996 504 140114',
+//     sms: 'Hello',
+//     sendTransfer: function (phoneNumber, sms) {
+//        console.log(phoneNumber, sms);
+//     }
+// }
+
+// console.log(user.sendTransfer('+996703406904', 'Darova zaibal)'));
+
